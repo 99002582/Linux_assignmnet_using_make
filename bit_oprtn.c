852a1966d49b128c622c0f5a25319709667656af
99002582 <71481186+99002582@users.noreply.github.com>
2020-10-13 11:20:42 +0530

    Add files via upload

---
+++ bit_oprtn.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "bit_oprtn.h"
+#include <stdbool.h>
+int set1 (int bit,int pos)
+{
+    bit |= (1 << pos);
+    return bit;
+}
+
+int reset(int bit)
+{
+    return(bit*0);
+}
+int flip(int bit)
+{
+    int temp=~bit;
+    return temp;
+}
+void isKthBitSet(int n, int k)
+{
+    if (n & (1 << (k - 1)))
+        printf( "\nSET");
+    else
+        printf("\nNOT SET");
+}
---
+++ bit_oprtn.h
@@ -0,0 +1,18 @@
+#ifndef BITMASK_H_INCLUDED
+#define BITMASK_H_INCLUDED
+
+#include <stdbool.h>
+
+int set1 (int bit,int pos);
+int reset(int bit);
+int flip(int bit);
+void isKthBitSet(int n, int k);
+
+#endif // BITMASK_H_INCLUDED
+/*
+#ifndef __BITMASK_H
+#define __BITMASK_H
+
+
+#endif // __BITMASK_H
+*/
