64cffdad0f7bb8243a2dfd6c02e672fa26bf4fcd
99002582 <71481186+99002582@users.noreply.github.com>
2020-10-23 09:53:43 +0530

    Update test.c

--- test.c
+++ test.c
@@ -5,12 +5,11 @@
 int main ()
 {
 	printf("\n........................................\n");
-	    int op=set1(4,4);
-	    int r=reset(2);
-	    int p=flip(4);
+	    int st=set1(4,4);
+	    int rst=reset(2);
+	    int flp=flip(4);
 
-	    printf("%d %d %d",op,r,p);
-	    printf(" ");
+	    printf("%d %d %d \n",st,rst,flp);
 	    isKthBitSet(3,5);
 	    int x,y;
 	    int count = 5;
@@ -32,18 +31,17 @@ int main ()
     int len = my_strlen(a);
     printf("Length is %d\n", len);
 
-    printf("\nCopying first string into second... \n");
-    my_strcpy(b, a); // copy the contents of ch_arr1 to ch_arr2
+    printf("\nCopying strings\n");
+    my_strcpy(b, a); 
     printf("First string (a) = %s\n", a);
     printf("Second string (b) = %s\n", b);
 
-    printf("\nConcatenating first and second string .. \n");
+    printf("\nConcatenating strings\n");
     my_strcat(a, b);
     printf("First string: %s\n", a);
     printf("Second string: %s\n\n", b);
 
-    //my_strcmp(a,b);
-     printf("Comparing first and second string .. ");
+    printf("Comparing strings");
     printf("\nstrcmp(\"a\", \"b\") = %d\n", my_strcmp("a", "b") );
 
 
