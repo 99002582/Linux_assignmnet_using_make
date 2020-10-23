af720044cd0ba2531488e105e045b6e6b257aa59
99002582 <71481186+99002582@users.noreply.github.com>
2020-10-23 09:56:51 +0530

    Update num_oprtn.h

--- num_oprtn.h
+++ num_oprtn.h
@@ -1,9 +1,9 @@
-#ifndef MYUTILS_H_INCLUDED
-#define MYUTILS_H_INCLUDED
+#ifndef NUM_OPRTN_H_INCLUDED
+#define NUM_OPRTN_H_INCLUDED
 
 int factorial(int);
 int isprime(int);
 int ispalindrome(int);
 int sum(int arg_count,...);
 
-#endif // MYUTILS_H_INCLUDED
+#endif // NUM_OPRTN_H_INCLUDED
